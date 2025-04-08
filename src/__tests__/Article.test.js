5ad4dfbd90c8ad3ed14d85592a9ab60ded58b50b
Hussein <hussein.osman.@student.moringaschool.com>
2025-04-08 09:06:39 +0300

    seve change complited

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -1,7 +1,6 @@
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import React from "react";
-
+import
+  { render, screen } from "@testing-library/react";
 import Article from "../components/Article";
 
 test("displays the text 'please pass this test'", () => {
